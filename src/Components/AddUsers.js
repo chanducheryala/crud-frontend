bea0b6ba91e461c662c7510bea72f6457aeeb104
chandu <cheryalachandu33@gmail.com>
2023-05-24 10:46:52 +0530

    crud mern

--- src/Components/AddUsers.js
+++ src/Components/AddUsers.js
@@ -1,106 +1,128 @@
-import React , { useState, useRef, useEffect}from 'react'
-import ViewUsers from './ViewUsers'
+import React, { useState, useRef, useEffect } from "react";
+import ViewUsers from "./ViewUsers";
+import axios from "axios";
 
 const AddUsers = () => {
-    const [details, setDetails]  = useState({ Name : " ", userName : " ", editIndex : -1})
-    const [list, setList] = useState([])
-    const[isDisabled, setIsDisabled] = useState(true); 
-    const nameRef = useRef();
-    
-    
-    useEffect(() =>{
-        let newName = String(details.Name).trim();
-        let newUserName = String(details.userName).trim();
-        if(newName.length !== 0  && newUserName.length !== 0) {
-             return setIsDisabled(false);
-        }else{
-            return setIsDisabled(true);
-        }   
-    },[details.Name, details.userName]
-    
-    )  
+    const BaseUrl = "http://localhost:8000/";
+  const [details, setDetails] = useState({ name: " ", userName: " " });
+  const [edit, setEdit] = useState({
+    status : false,
+    _id : " "
+  });
+  const [list, setList] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [call, setCall] = useState(false);
+  const nameRef = useRef();
+
+  useEffect(() => {
+    let newName = String(details.name).trim();
+    let newUserName = String(details.userName).trim();
+    if (newName.length !== 0 && newUserName.length !== 0) {
+      return setIsDisabled(false);
+    } else {
+      return setIsDisabled(true);
+    }
+  }, [details.name, details.userName]);
+
+  useEffect(() => {
    
+   const callToFetch = async() => {
+    try {
+        const resp = await axios.get(BaseUrl);
+        setList(resp.data);
+        setCall(false);
+      } catch (e) {
+        console.log(e.message);
+      }
+   }
+    callToFetch()
+  }, [call]);
 
-    const submitHandler = (e) =>{
-        e.preventDefault();
-        
-        let newName = String(details.Name).trim();
-        let newUserName = String(details.userName).trim();
-    
-       if(newName.length !== 0  && newUserName.length !== 0) {     
-          
-        if(details.editIndex === -1 ){ 
-            setList([...list,{...details, Name : details.Name, userName : details.userName}])
-            setDetails({...details,Name : ' ', userName:' '})
-            setIsDisabled(true)
-            
-         } else {      
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-             const newList = list.map((person,index) => {
-              if(index === details.editIndex){
-                  return {...person, Name : details.Name, userName : details.userName}
-              }
-              else{
-                  return person;
-              }
-  
-             })
-             setDetails({...details,Name : ' ', userName:' ', editIndex : -1})
-                setList(newList);
-                
-         }
-       }
-        
+    let newName = String(details.name).trim();
+    let newUserName = String(details.userName).trim();
+
+    if (newName.length !== 0 && newUserName.length !== 0) {
+      if (edit.status === false) {
+        await axios
+          .post(BaseUrl, details)
+          .then((resp) => console.log(resp))
+          .catch((e) => console.log(e.message));
+        setIsDisabled(true);
+        setDetails({ ...details, name: " ", userName: " " });
+      } else {
+        await axios
+          .put(BaseUrl, {...details, _id : edit._id })
+        setDetails({ ...details, name: " ", userName: " " });
+        setEdit({...edit, status : false, _id : " "});
+      }
     }
+    setCall(true);
+  };
 
+  const editHandler = async (userId) => {
+    setEdit({...edit, status : true, _id : userId});
+    const resp = await axios.get(`${BaseUrl}${userId}`);
+    const user = await resp.data;
+    setDetails({ ...details, name: user.name, userName: user.userName });
+  };
 
-    const editHandler = (indexValue) => {
-        
-        nameRef.current.focus();
-        const TargetObj = list.filter((person, index) => index === indexValue) 
-        const TargetName = TargetObj.map(person => person.Name);
-        const TargetUserName = TargetObj.map(person => person.userName);
-       
-       setDetails({...details, Name : TargetName, userName : TargetUserName, editIndex : indexValue})
-        
-       
+  const deleteHandler = async (personId) => {
+    console.log({personId});
+    const resp = await axios.delete(`${BaseUrl}${personId}`);
+    console.log(resp.data);
+    setCall(true);
+  };
 
-    }
-
-    const deleteHandler = (indexValue) => {
-      const modifiedList = list.filter((person, index) => index !== indexValue)
-      setDetails({...details, Name : ' ', userName : ' ' , editIndex : -1});  
-      setIsDisabled(true);
-      setList(modifiedList);
-    }
-    
-     const changeHandler = (e) => {
-        setDetails({...details, [e.target.name] : e.target.value})
-     }
+  const changeHandler = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div className='main-container'>
-       <div className='input-container'>
-            <header className='input-header'>Add user</header>
-            <form onSubmit={submitHandler} className = 'form-container'>
-           
-                <div className='input-elements'>
-                    <label className='label'>Name</label>
-                    <input type = "text"  name = 'Name' value = {details.Name} onChange = {changeHandler}  ref = {nameRef}  className = 'text-fields' required/>
-                </div>
-                <div className='input-elements'>
-                    <label className='label'>Username</label>
-                    <input type = "text"  name = 'userName' value = {details.userName} onChange = {changeHandler} className = 'text-fields' required/>
-                </div>
-                <div >
-                        <button type = 'submit' disabled = {isDisabled} className='submit-btn'>submit</button>
-                </div>
-            </form>
-       </div>
+    <div className="main-container">
+      <div className="input-container">
+        <header className="input-header">Add user</header>
+        <form onSubmit={submitHandler} className="form-container">
+          <div className="input-elements">
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="name"
+              value={details.name}
+              onChange={changeHandler}
+              ref={nameRef}
+              className="text-fields"
+              required
+            />
+          </div>
+          <div className="input-elements">
+            <label className="label">Username</label>
+            <input
+              type="text"
+              name="userName"
+              value={details.userName}
+              onChange={changeHandler}
+              className="text-fields"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit" disabled={isDisabled} className="submit-btn">
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
 
-       <ViewUsers editHandler = {editHandler} deleteHandler = {deleteHandler} list = {list}/>
+      <ViewUsers
+        editHandler={editHandler}
+        deleteHandler={deleteHandler}
+        list={list}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddUsers
+export default AddUsers;
--- src/Components/ViewUsers.js
+++ src/Components/ViewUsers.js
@@ -1,46 +1,58 @@
-import React from 'react'
+import React from "react";
 
-const ViewUsers = ({editHandler,deleteHandler, list}) => {
+const ViewUsers = ({ editHandler, deleteHandler, list }) => {
+    const headings = () => {
+        return list.length === 0 ? (
+            <div> </div>
+        ) : (
+            <div className="titles">
+                <header className="title-name">Name</header>
+                <header className="title-name">Username</header>
+                <header className="title-name">Actions</header>
+            </div>
+        );
+    };
 
+    return (
+        <div className="output-container">
+            <div>
+                <header className="output-header">View Users</header>
+            </div>
 
-   const headings = () => {
-    
-    return (list.length === 0) ? <div> </div> :
-       <div className='titles'>
-            <header className = 'title-name'>Name</header>
-            <header className = 'title-name'>Username</header>
-            <header className = 'title-name'>Actions</header>
+            {headings()}
+
+            <div className="user-details">
+                {list ? list?.map((person) => {
+                    return (
+                        <div key={person._id} className="user">
+                            <span className="display-name">{person.name}</span>
+                            <span className="display-userName">{person.userName}</span>
+                            <div className="display-btn">
+                                <button
+                                    onClick={() => editHandler(person._id)}
+                                    className="action-btn"
+                                    style={{
+                                        marginRight: "0.2em",
+                                        fontWeight: "550",
+                                        height: "3em",
+                                    }}
+                                >
+                                    Edit
+                                </button>
+                                <button
+                                    onClick={() => deleteHandler(person._id)}
+                                    className="action-btn"
+                                    style={{ fontWeight: "550", height: "3em" }}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    );
+                }) : <></>}
+            </div>
         </div>
-       
-     
-   }
-    
-  return (
-    <div className='output-container'>
-        <div>
-            <header className = 'output-header'>View Users</header>
-        </div>
+    );
+};
 
-           {headings()}
-       
-        <div className='user-details'>
-             {
-                list.map((person, index) => {
-                    return <div key  = {index}  className='user'>
-                              <span className = 'display-name'>{person.Name}</span>
-                              <span className = 'display-userName'>{person.userName}</span>
-                              <div className='display-btn'>
-                                 <button onClick={()=> editHandler(index)} className= 'action-btn' style={{marginRight: '0.2em' , fontWeight: '550', height: '3em'}}>Edit</button>
-                                 <button onClick={()=> deleteHandler(index)} className= 'action-btn' style={{fontWeight: '550',height: '3em'}}>Delete</button>
-                              </div>
-                            
-                           </div>
-                })
-            }
-        </div>
-       
-    </div>
-  )
-}
-
-export default ViewUsers
+export default ViewUsers;
